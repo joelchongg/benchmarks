02157eb5d5d5ee681655960a54439bd6b51cc94e
joelchongg <joelchongggg@gmail.com>
2026-07-19 23:13:57 +0800

    [fix]: io uring implementation

--- io_benchmarks/benchmarks/bench_io_uring.cpp
+++ io_benchmarks/benchmarks/bench_io_uring.cpp
@@ -7,9 +7,9 @@
 #include <unistd.h>
 #include <vector>
 
-constexpr size_t BLOCK_SZ = 4096;
-constexpr unsigned int QUEUE_DEPTH = 8;
-constexpr size_t TOTAL_BUFFER_SIZE = BLOCK_SZ * QUEUE_DEPTH * 2;
+constexpr size_t BLOCK_SZ = 256 * 1024;
+constexpr unsigned int QUEUE_DEPTH = 32;
+constexpr size_t TOTAL_BUFFER_SIZE = BLOCK_SZ * QUEUE_DEPTH;
 
 struct RequestContext {
     uint8_t* buffer;
@@ -36,63 +36,43 @@ int main(int argc, char* argv[]) {
     }
 
     struct io_uring ring;
-    if (io_uring_queue_init(QUEUE_DEPTH * 2, &ring, 0) < 0) [[unlikely]] {
+    if (io_uring_queue_init(QUEUE_DEPTH, &ring, 0) < 0) [[unlikely]] {
         throw std::runtime_error("io_uring_queue_init failed.");
     }
 
     // perform software pipelining of data
-    size_t offset = 0;
-    uint8_t in_flight = 0;
-    std::vector<RequestContext> reqs(QUEUE_DEPTH * 2);
-    for (unsigned int i = 0; i < QUEUE_DEPTH * 2; ++i) {
+    off_t offset = 0;
+    size_t total_outstanding = 0;
+    std::vector<RequestContext> reqs(QUEUE_DEPTH);
+    for (unsigned int i = 0; i < QUEUE_DEPTH; ++i) {
         reqs[i].buffer = static_cast<uint8_t*>(buffer) + (i * BLOCK_SZ);
-    }
-
-    for (int i = 0; i < QUEUE_DEPTH; ++i) {
         struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
-        size_t read_size = std::min(BLOCK_SZ, st.st_size - offset);
+        size_t read_size = std::min(static_cast<off_t>(BLOCK_SZ), st.st_size - offset);
 
         io_uring_prep_read(sqe, fd, reqs[i].buffer, read_size, offset);
         io_uring_sqe_set_data(sqe, &reqs[i]);
 
         reqs[i].offset = offset;
         offset += read_size;
-        ++in_flight;
+        ++total_outstanding;
     }
     io_uring_submit(&ring);
 
     size_t total_bytes_read = 0;
-    size_t start_sqes_idx = QUEUE_DEPTH;
     std::vector<size_t> histogram(256, 0);
-    while (total_bytes_read < st.st_size) {
+
+    while (total_outstanding > 0) {
         struct io_uring_cqe* cqes;
 
-        int ret = io_uring_wait_cqes(&ring, &cqes, in_flight, NULL, NULL);
+        int ret = io_uring_wait_cqes(&ring, &cqes, 1, NULL, NULL);
         if (ret < 0) [[unlikely]] {
             throw std::runtime_error("io_uring_wait_cqes failed");
         }
 
-        int next_in_flight = 0;
-        // pipeline the next QUEUE_DEPTH requests first before processing
-        for (int i = 0; i < QUEUE_DEPTH && offset < st.st_size; ++i) {
-            struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
-            size_t read_size = std::min(BLOCK_SZ, st.st_size - offset);
-            
-            io_uring_prep_read(sqe, fd, reqs[start_sqes_idx + i].buffer, read_size, offset);
-            io_uring_sqe_set_data(sqe, &reqs[start_sqes_idx + i]);
-
-            reqs[start_sqes_idx + i].offset = offset;
-            offset += read_size;
-            next_in_flight++;
-        }
-        if (next_in_flight > 0) {
-            io_uring_submit(&ring);
-        }
-        start_sqes_idx ^= QUEUE_DEPTH;
-
         // process data
         unsigned int head;
         unsigned int processed_count = 0;
+        unsigned int to_submit = 0;
         io_uring_for_each_cqe(&ring, head, cqes) {
             if (cqes->res < 0) [[unlikely]] {
                 throw std::runtime_error("io_uring async get failed: " + std::string(strerror(-cqes->res))); 
@@ -106,10 +86,27 @@ int main(int argc, char* argv[]) {
                 histogram[req->buffer[j]]++;
             } 
             ++processed_count;
+
+            if (offset < st.st_size) {
+                struct io_uring_sqe* sqe = io_uring_get_sqe(&ring);
+                size_t read_size = std::min(static_cast<off_t>(BLOCK_SZ), st.st_size - offset);
+
+                io_uring_prep_read(sqe, fd, req->buffer, read_size, offset);
+                io_uring_sqe_set_data(sqe, req);
+                
+                req->offset = offset;
+                offset += read_size;
+                to_submit++;
+            }
         };
 
         io_uring_cq_advance(&ring, processed_count);
-        in_flight = next_in_flight;
+        total_outstanding -= processed_count;
+
+        if (to_submit > 0) {
+            io_uring_submit(&ring);
+            total_outstanding += to_submit;
+        }
     }
 
     io_uring_queue_exit(&ring);
