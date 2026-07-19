641208c0bc2dadeaadf03963a556ab55583e5d06
joelchongg <joelchongggg@gmail.com>
2026-07-19 22:47:02 +0800

    [feat]: add dependency

--- io_benchmarks/scripts/install_deps.sh
+++ io_benchmarks/scripts/install_deps.sh
@@ -5,6 +5,9 @@ echo "Installing required dependencies..."
 
 sudo apt update
 
+# perf
+sudo apt install -y linux-tools-common linux-tools-generic linux-tools-$(uname -r)
+
 # io uring helper library
 sudo apt install -y build-essential cmake liburing-dev
 
